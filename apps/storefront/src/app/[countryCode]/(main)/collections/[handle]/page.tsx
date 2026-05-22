61b73d1f575e0f17c7c038456965a5111a2e9e49
Shazin SV <muhammedshazin354@gmail.com>
2026-05-22 20:38:53 +0530

    incoming3

--- apps/storefront/src/app/[countryCode]/(main)/collections/[handle]/page.tsx
+++ apps/storefront/src/app/[countryCode]/(main)/collections/[handle]/page.tsx
@@ -1,8 +1,6 @@
-import { getCollectionByHandle, listCollections } from "@/lib/data/collections"
-import { listRegions } from "@/lib/data/regions"
+import { getCollectionByHandle } from "@/lib/data/collections"
 import CollectionTemplate from "@/modules/collections/templates"
 import { SortOptions } from "@/modules/store/components/refinement-list/sort-products"
-import { StoreCollection, StoreRegion } from "@medusajs/types"
 import { Metadata } from "next"
 import { notFound } from "next/navigation"
 
@@ -18,42 +16,6 @@ type Props = {
     sortBy?: SortOptions
     q?: string
   }>
-}
-
-export const PRODUCT_LIMIT = 12
-
-export async function generateStaticParams() {
-  const { collections } = await listCollections({
-    offset: "0",
-    limit: "100",
-  })
-
-  if (!collections) {
-    return []
-  }
-
-  const countryCodes = await listRegions().then(
-    (regions: StoreRegion[]) =>
-      regions
-        ?.map((r) => r.countries?.map((c) => c.iso_2))
-        .flat()
-        .filter(Boolean) as string[]
-  )
-
-  const collectionHandles = collections.map(
-    (collection: StoreCollection) => collection.handle
-  )
-
-  const staticParams = countryCodes
-    ?.map((countryCode: string) =>
-      collectionHandles.map((handle: string | undefined) => ({
-        countryCode,
-        handle,
-      }))
-    )
-    .flat()
-
-  return staticParams
 }
 
 export async function generateMetadata(props: Props): Promise<Metadata> {
